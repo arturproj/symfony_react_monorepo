f8c0bc8335edf132b3efe7517de3caec65861e97
Artur <artur.ddwebproject@gmail.com>
2026-08-26 17:27:44 +0200

    Final commit

---
+++ assets/controllers/app_controller.js
@@ -0,0 +1,19 @@
+// assets/controllers/react_controller.js
+import { Controller } from '@hotwired/stimulus';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../react/components/AppComponent/App';
+
+export default class extends Controller {
+    static values = { props: Object };
+
+    connect() {
+        const root = createRoot(this.element);
+        root.render(<App {...this.propsValue} />);
+        this.root = root;
+    }
+
+    disconnect() {
+        this.root?.unmount();
+    }
+}
